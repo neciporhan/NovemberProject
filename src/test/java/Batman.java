f5050c6d0380e2484dfc113de1c516ee7312b60c
neciporhan <neciporhan@gmail.com>
2020-11-01 09:23:39 -0700

    Batman have BatMobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("I will catch you Joker");
         // yes he will catch him
+        // Batman has cool Batmobile
     }
 }
